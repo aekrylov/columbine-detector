1c7909ccbe1e6564a05ae673732b20a66b68248b
Anton Krylov <krylov@cg.ru>
2019-10-22 00:11:39 +0300

    Haar model minor improvements

--- object_detection_haar.py
+++ object_detection_haar.py
@@ -5,6 +5,14 @@ import datetime
 
 gun_cascade = cv2.CascadeClassifier('cascade.xml')
 camera = cv2.VideoCapture('data/gun4_2.mp4')
+fps = int(camera.get(cv2.CAP_PROP_FPS))
+width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+targetWidth = 480
+scaleFactor = width // targetWidth
+targetHeight = height // scaleFactor
+
 outputFile = "out.avi"
 
 # initialize the first frame in the video stream
@@ -14,7 +22,7 @@ firstFrame = None
 
 gun_exist = False
 
-vid_writer = cv2.VideoWriter(outputFile, cv2.VideoWriter_fourcc('M','J','P','G'), 30, (round(camera.get(cv2.CAP_PROP_FRAME_WIDTH)),round(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+vid_writer = cv2.VideoWriter(outputFile, cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 30, (targetWidth, targetHeight))
 
 while True:
     (grabbed, frame) = camera.read()
@@ -24,34 +32,33 @@ while True:
         break
 
     # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
+    frame = imutils.resize(frame, width=targetWidth)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
-    
-    gun = gun_cascade.detectMultiScale(gray, 1.3, 5, minSize = (100, 100))
-    
+
+    gun = gun_cascade.detectMultiScale(gray, 1.3, 5, minSize=(100, 100))
+
     if len(gun) > 0:
         gun_exist = True
-        
-    for (x,y,w,h) in gun:
-        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]    
 
-    # if the first frame is None, initialize it
+    for (x, y, w, h) in gun:
+        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        # if the first frame is None, initialize it
     if firstFrame is None:
         firstFrame = gray
         continue
 
     # draw the text and timestamp on the frame
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-					
     vid_writer.write(frame.astype(np.uint8))
     # show the frame and record if the user presses a key
     cv2.imshow("Security Feed", frame)
-    key = cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(1000 // fps) & 0xFF  # Simulate 24 fps
 
 if gun_exist:
     print("guns detected")
